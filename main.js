8df148856f28c8cd4702ff2391a1b60edfb1af96
ElvinCCTV <qadirov.elvin.qe@gmail.com>
2024-06-01 22:04:59 +0400

    Iterations are ready too

--- main.js
+++ main.js
@@ -26,6 +26,11 @@ function showContentTwo(buttonElement) {
 const energy = new Date(2026, 9, 27);
 const birth = new Date(2001, 9, 27);
 const death = new Date(2057, 9, 27);
+// PROJECTS
+const iterationOne = new Date(2024, 5, 1);
+const app = new Date(2024, 5, 10);
+const start = new Date(2024, 3, 17);
+const graduation = new Date(2024, 7, 15);
 
 function calc() {
 	const now = new Date();
@@ -37,32 +42,71 @@ function calc() {
 	let remaining_life_percent = (ramainingTime / totalTime) * 100;
 	let remaining_energy_percent = (ramainingEnergy / totalEnergyTime) * 100;
 
-    const timeTillDeath = showTime(ramainingTime);
-    const energyLeft = showTime(ramainingEnergy);
+	// PROJECTS
+	let totalTillApp = app - iterationOne;
+	let totalTillGraduatuon = graduation - start;
+	let remainingTillApp = app - now;
+	let remainingTillGraduation = graduation - now;
+	let remainingTillAppPr = (remainingTillApp / totalTillApp) * 100;
+	let remainingTillGraduationPr =
+		(remainingTillGraduation / totalTillGraduatuon) * 100;
 
+	const timeTillDeath = showTime(ramainingTime);
+	const energyLeft = showTime(ramainingEnergy);
+	const tillApp = showTime(remainingTillApp);
+	const tillGraduation = showTime(remainingTillGraduation);
 
-    const elements = ['weeks', 'months', 'hours', 'minutes', 'seconds'];
-    const suffixes = ['|W', '|M', '|H', '|min', '|Sec'];
+	const elements = ["months", "weeks", "days", "minutes", "seconds"];
+	const suffixes = ["|W", "|M", "|D", "|min", "|Sec"];
 
-    for(let i = 0; i < elements.length; i++){
-        document.getElementById(elements[i]).innerHTML = timeTillDeath[i] + suffixes[i];
-        document.getElementById(elements[i] + '-two').innerHTML = energyLeft[i] + suffixes[i];
-    }
-    document.getElementById('zaryad').style.height = remaining_life_percent + '%';
-    document.getElementById('Energy').style.height = remaining_energy_percent + '%';
+	for (let i = 0; i < elements.length; i++) {
+		if (document.title === `CMP`) {
+			document.getElementById(elements[i]).innerHTML =
+				timeTillDeath[i] + suffixes[i];
 
+			document.getElementById(elements[i] + "-two").innerHTML =
+				energyLeft[i] + suffixes[i];
+		}
+		if (document.title === `Elvin`) {
+			document.getElementById(elements[i] + "-iteration").innerHTML =
+				tillApp[i] + suffixes[i];
+			document.getElementById(elements[i] + "-graduation").innerHTML =
+				tillGraduation[i] + suffixes[i];
+		}
+	}
+
+	if (document.title === `CMP`) {
+		document.getElementById("zaryad").style.height =
+			remaining_life_percent + "%";
+
+		document.getElementById("Energy").style.height =
+			remaining_energy_percent + "%";
+		document.getElementById("PrOne").innerHTML =
+			remaining_life_percent.toPrecision(4) + "%";
+		document.getElementById("PrTwo").innerHTML =
+			remaining_energy_percent.toPrecision(3) + "%";
+	}
+	if (document.title === `Elvin`) {
+		document.getElementById("zaryad").style.height =
+		remainingTillAppPr + "%";
+
+		document.getElementById("Energy").style.height =
+		remainingTillGraduationPr + "%";
+		document.getElementById("PrOne").innerHTML =
+			remainingTillAppPr.toPrecision(4) + "%";
+		document.getElementById("PrTwo").innerHTML =
+			remainingTillGraduationPr.toPrecision(3) + "%";
+	}
 }
 
 function showTime(defference) {
 	let hour = 1000 * 60 * 60;
 	let weeks = Math.floor(defference / (hour * 24 * 7));
 	let months = Math.floor(defference / (hour * 24 * 30.5));
-	let hours = Math.floor(
-		(defference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-	);
-	let minutes = Math.floor((defference % (1000 * 60 * 60)) / (1000 * 60));
+	let days = Math.floor(defference / (hour * 24));
+	let minutes = Math.floor((defference % hour) / (1000 * 60));
 	let seconds = Math.floor((defference % (1000 * 60)) / 1000);
-	timeArray = [weeks, months, hours, minutes, seconds];
+	timeArray = [weeks, months, days, minutes, seconds];
 	return timeArray;
 }
 
